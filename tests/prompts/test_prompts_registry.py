8d793977245d2b7b71ef77c163de663854ae9ad8
JosePizarro3 <jose.pizarro-blanco@bam.de>
2025-10-13 15:15:27 +0200

    Fixed test_PROMPT_REGISTRY

--- tests/prompts/test_prompts_registry.py
+++ tests/prompts/test_prompts_registry.py
@@ -5,7 +5,7 @@ from nerxiv.prompts import PROMPT_REGISTRY
 
 def test_PROMPT_REGISTRY():
     registered_queries = set(PROMPT_REGISTRY.keys())
-    expected_queries = {"material_formula", "material_formula_structured"}
+    expected_queries = {"material_formula", "material_formula_structured", "only_dmft"}
     if registered_queries != expected_queries:
         pytest.fail(
             f"`PROMPT_REGISTRY` query keys have changed.\n"
