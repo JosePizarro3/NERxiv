6ac7feba1eccfd9cc56eefa4850f11c95aebf524
JosePizarro3 <jose.pizarro-blanco@bam.de>
2025-10-31 15:07:00 +0100

    Clean up init modules

--- nerxiv/rag/__init__.py
+++ nerxiv/rag/__init__.py
@@ -1,16 +1,3 @@
-# ############################################################################
-# This sub-folder contains the retrieval and generation classes
-# and functions to process the text. These are used to retrieve
-# relevant chunks of text and generate structured output using
-# an LLM.
-# ############################################################################
-
-"""RAG subpackage.
-
-This package contains retriever/generator implementations. Importing the
-submodules directly (for example `from nerxiv.rag import retriever`) is the
-recommended pattern to avoid pulling optional dependencies at package import
-time.
-"""
-
-__all__ = []
+from .agent import RAGExtractorAgent
+from .generator import LLMGenerator
+from .retriever import CustomRetriever, LangChainRetriever
